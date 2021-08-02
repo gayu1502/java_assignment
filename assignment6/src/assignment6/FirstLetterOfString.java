a0cba3e6216128a363c4fe2f3937c04e9a279d91
gayu1502 <87376160+gayu1502@users.noreply.github.com>
2021-08-02 14:08:31 +0530

    Add files via upload

---
+++ assignment6/src/assignment6/FirstLetterOfString.java
@@ -0,0 +1,18 @@
+package assignment6;
+
+public class FirstLetterOfString {
+
+	public static void main(String[] args) {
+		
+		String[] array= {"Gayatri","Dinesh","Patil"};
+		
+		for(int i=0;i<array.length;i++) {
+			String s=array[i];
+			System.out.print(s.charAt(0));
+			
+		}
+		
+	}
+
+	
+}
---
+++ assignment6/src/assignment6/PatternFifth.java
@@ -0,0 +1,15 @@
+package assignment6;
+
+public class PatternFifth {
+
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(i);
+			}
+		System.out.println();
+		}
+	
+	}
+
+}
